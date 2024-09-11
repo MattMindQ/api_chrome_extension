42b57e2e7db8254ed91d7ae3980d5fb1b50b8899
MattMindQ <dubrulle.matthieu@gmail.com>
2024-09-11 17:13:47 +0200

    ssl update

--- database.py
+++ database.py
@@ -3,11 +3,12 @@ from motor.motor_asyncio import AsyncIOMotorClient
 from pymongo.server_api import ServerApi
 import os
 from dotenv import load_dotenv
+import ssl
 
 load_dotenv()
 
 uri = os.getenv("MONGODB_URI")
-client = AsyncIOMotorClient(uri, server_api=ServerApi('1'))
+client = AsyncIOMotorClient(uri, server_api=ServerApi('1'), ssl=True, ssl_cert_reqs=ssl.CERT_NONE)
 db = client.company_data
 
 async def create_or_update_company(company_data):
